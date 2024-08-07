ca8abcf0c3b6334aeac9901dcf56228a5faf5cd2
simple-solana-app <simplesolanaapp@gmail.com>
2024-08-07 13:31:39 +0200

    boring job

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,44 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    let instruction = SimpleInstructions::unpack(instruction_data)?;
+
+    match instruction {
+        SimpleInstructions::InitRequiredProgramAccounts => {
+            
+        }
+        SimpleInstructions::InitRequiredUserAccountsAndExecute {
+            has_claim_account,
+            has_simple_token_account,
+        } => match (has_claim_account, has_simple_token_account) {
+            (false, true) => initialize_user_claim_tracker_account(),
+            (true, false) => initialize_user_simple_token_account(),
+            (false, false) => initialize_user_accounts(),
+            (true, true) => execute(),
+        },
+        SimpleInstructions::Execute => {
+            execute();
+        }
+    }
     Ok(())
+}
+
+pub fn execute() {}
+
+pub fn initialize_user_claim_tracker_account() {}
+
+pub fn initialize_user_simple_token_account() {}
+
+pub fn initialize_user_accounts() {
+    initialize_user_claim_tracker_account();
+    initialize_user_simple_token_account();
 }
 
 pub enum SimpleInstructions {
     InitRequiredProgramAccounts,
     InitRequiredUserAccountsAndExecute {
         has_claim_account: bool,
-        has_ass_simple_token_account: bool,
+        has_simple_token_account: bool,
     },
     Execute,
 }
@@ -26,7 +56,7 @@ pub enum SimpleInstructions {
 #[derive(BorshDeserialize)]
 struct SimpleInstructionsPayload {
     has_claim_account: bool,
-    has_ass_simple_token_account: bool,
+    has_simple_token_account: bool,
 }
 
 impl SimpleInstructions {
@@ -41,7 +71,7 @@ impl SimpleInstructions {
             0 => Self::InitRequiredProgramAccounts,
             1 => Self::InitRequiredUserAccountsAndExecute {
                 has_claim_account: user_info.has_claim_account,
-                has_ass_simple_token_account: user_info.has_ass_simple_token_account,
+                has_simple_token_account: user_info.has_simple_token_account,
             },
             2 => Self::Execute,
             _ => return Err(ProgramError::InvalidInstructionData),
