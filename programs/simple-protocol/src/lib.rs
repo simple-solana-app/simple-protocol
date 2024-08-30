1d953e1b106ea3b4ff95a0690e3fa9a44b8cfd8a
simple-solana-app <simplesolanaapp@gmail.com>
2024-08-30 12:55:34 +0200

    got it

--- programs/simple-protocol/src/lib.rs
+++ programs/simple-protocol/src/lib.rs
@@ -1,26 +1,152 @@
 use std::str::FromStr;
 
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, TokenAccount};
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("DyFiYpt5vU7zeFS3UDs2gq7HuFNBarwMi8PwjKFfbumS");
 
 pub const SIMPLE_PUBKEY: &str = "E61fUAd1cxFES9kPckPhzwiiFMRo8ezAw7ZG5a8YD2jv";
-pub const SIMPLE_MINT: &str = "9CigozmpiDkUCXBjWojV1hi4jj6Sc47LXfs3aXKjhv2j";
-
+pub const SIMPLE_MINT: &str = "GL3E99ERZBe68mYXrJZfEoSWwoY4QbCWT5H6Jvb7E5RC";
 
 #[program]
 pub mod simple_protocol {
+    use anchor_spl::token::{transfer, Transfer};
+
     use super::*;
 
-    pub fn initialize(ctx: Context<InitializeProgramAccounts>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_most_program_accounts(
+        ctx: Context<InitializeMostProgramAccounts>,
+    ) -> Result<()> {
+        let percent_tracker = &mut ctx.accounts.percent_tracker;
+        percent_tracker.increment = 0;
+
+        let wsol_balance = &mut ctx.accounts.wsol_balance;
+        wsol_balance.amount = 0;
+        Ok(())
+    }
+
+    pub fn initialize_remaining_program_accounts(
+        ctx: Context<InitializeRemainingProgramAccounts>,
+    ) -> Result<()> {
+        let transfer_authority = &ctx.accounts.transfer_authority;
+        let program_simple_account = &ctx.accounts.program_simple_token;
+
+        msg!(
+            "transfer_authority {} ({})",
+            transfer_authority.key(),
+            transfer_authority.to_account_info().owner
+        );
+        msg!(
+            "program_simple_account {} ({}): {}",
+            program_simple_account.key(),
+            program_simple_account.to_account_info().owner,
+            program_simple_account.amount
+        );
+
+        Ok(())
+    }
+
+    pub fn execute(ctx: Context<Execute>) -> Result<()> {
+        let percent_tracker = &mut ctx.accounts.percent_tracker;
+        let wsol_balance = &mut ctx.accounts.wsol_balance;
+
+        let transfer_authority = &ctx.accounts.transfer_authority;
+        let bump_seed = ctx.bumps.transfer_authority;
+        let signer_seeds: &[&[&[u8]]] = &[&[b"transfer_authority", &[bump_seed]]];
+        let program_simple_account = &mut ctx.accounts.program_simple_token;
+
+        let dest = &mut ctx.accounts.dest;
+
+        let token_program = &ctx.accounts.token_program;
+
+        transfer(
+            CpiContext::new_with_signer(
+                token_program.to_account_info(),
+                Transfer {
+                    from: program_simple_account.to_account_info(),
+                    to: dest.to_account_info(),
+                    authority: transfer_authority.to_account_info(),
+                },
+                signer_seeds,
+            ),
+            10,
+        )?;
+
+        msg!(
+            "percent_tracker {} ({}): {}",
+            percent_tracker.key(),
+            percent_tracker.to_account_info().owner,
+            percent_tracker.increment
+        );
+        msg!(
+            "wsol_balance {} ({}): {}",
+            wsol_balance.key(),
+            wsol_balance.to_account_info().owner,
+            wsol_balance.amount
+        );
+        msg!(
+            "transfer_authority {} ({})",
+            transfer_authority.key(),
+            transfer_authority.to_account_info().owner
+        );
+        msg!(
+            "program_simple_account {} ({}): {}",
+            program_simple_account.key(),
+            program_simple_account.to_account_info().owner,
+            program_simple_account.amount
+        );
+        msg!(
+            "dest {} ({}): {}",
+            dest.key(),
+            dest.to_account_info().owner,
+            dest.amount
+        );
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-struct InitializeProgramAccounts<'info> {
+pub struct Execute<'info> {
+    #[account(mut)]
+    user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"percent_tracker"],
+        bump
+    )]
+    percent_tracker: Account<'info, Tracker>,
+    #[account(
+        mut,
+        seeds = [b"wsol_balance"],
+        bump
+    )]
+    wsol_balance: Account<'info, Balance>,
+    #[account(
+        mut,
+        seeds = [b"transfer_authority"],
+        bump
+    )]
+    transfer_authority: Account<'info, Authority>,
+    #[account(address = Pubkey::from_str(SIMPLE_MINT).unwrap())]
+    simple_mint: Account<'info, Mint>,
+    #[account(
+        mut,
+        token::mint = simple_mint,
+        token::authority = transfer_authority,
+        seeds = [b"program_simple_token_account"],
+        bump
+    )]
+    program_simple_token: Account<'info, TokenAccount>,
+    token_program: Program<'info, Token>,
+    #[account(
+        mut,
+    )]
+    dest: Account<'info, TokenAccount>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeMostProgramAccounts<'info> {
     #[account(
         mut,
         address = Pubkey::from_str(SIMPLE_PUBKEY).unwrap()
@@ -42,6 +168,16 @@ struct InitializeProgramAccounts<'info> {
         bump
     )]
     wsol_balance: Account<'info, Balance>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeRemainingProgramAccounts<'info> {
+    #[account(
+        mut,
+        address = Pubkey::from_str(SIMPLE_PUBKEY).unwrap()
+    )]
+    simple: Signer<'info>,
     #[account(
         init,
         payer = simple,
@@ -57,21 +193,23 @@ struct InitializeProgramAccounts<'info> {
         payer = simple,
         token::mint = simple_mint,
         token::authority = transfer_authority,
+        seeds = [b"program_simple_token_account"],
+        bump
     )]
     program_simple_token: Account<'info, TokenAccount>,
-    system_program: Program<'info, System>
-    
+    token_program: Program<'info, Token>,
+    system_program: Program<'info, System>,
 }
 
 #[account]
-pub struct Tracker {
+struct Tracker {
     increment: u8,
 }
 
 #[account]
-pub struct Balance {
+struct Balance {
     amount: u64,
 }
 
 #[account]
-pub struct Authority {}
+struct Authority {}
--- tests/simple-protocol.ts
+++ tests/simple-protocol.ts
@@ -1,15 +1,82 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SimpleProtocol } from "../target/types/simple_protocol";
+import { PublicKey } from "@solana/web3.js";
+import * as fs from "fs";
+
+
+
 
 describe("simple-protocol", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.SimpleProtocol as Program<SimpleProtocol>;
+  const SIMPLE = Uint8Array.from(JSON.parse(fs.readFileSync("/home/seb/MY/KEYS/simple.json", "utf-8")));
+  const SIMPLE_KEPPAIR = anchor.web3.Keypair.fromSecretKey(SIMPLE);
+  const SIMPLE_MINT = new PublicKey("GL3E99ERZBe68mYXrJZfEoSWwoY4QbCWT5H6Jvb7E5RC");
+  const SIMPLE_SIMPLE_TOKEN_ACCOUNT = new PublicKey("7SaGM8pKVz5JipgUT8LNkPuncXRoTKpkW7TSa3qkMxHY");
 
-  it("Program pdas initialized", async () => {
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  const [percentTrackerPda, percentTrackerBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from("percent_tracker")],
+    program.programId
+  );
+
+ const [wsolBalancePda, wsolBalanceBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from("wsol_balance")],
+    program.programId
+  );
+
+ const [transferAuthorityPda, transferAuthorityBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from("transfer_authority")],
+    program.programId
+  );
+
+ const [programSimpleTokenPda, programSimpleTokenBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from("program_simple_token_account")],
+    program.programId
+  );
+
+    // it("Most Program PDAs initialized", async () => {
+    //   const tx = await program.methods.initializeMostProgramAccounts()
+    //     .accounts({
+    //       simple: SIMPLE_KEPPAIR.publicKey,
+    //       percentTracker: percentTrackerPda,
+    //       wsolBalance: wsolBalancePda,
+    //       systemProgram: anchor.web3.SystemProgram.programId,
+    //     })
+    //     .signers([SIMPLE_KEPPAIR]) // Add the loaded keypair as the signer
+    //     .rpc();
+    
+    //   console.log("Transaction signature:", tx);
+    // });
+    // it("Remaining Program PDAs initialized", async () => {
+    //   const tx = await program.methods.initializeRemainingProgramAccounts()
+    //     .accounts({
+    //       simple: SIMPLE_KEPPAIR.publicKey,
+    //       transferAuthority: transferAuthorityPda,
+    //       simpleMint: SIMPLE_MINT,
+    //       programSimpleToken: programSimpleTokenPda,
+    //       tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID, 
+    //       systemProgram: anchor.web3.SystemProgram.programId,
+    //     })
+    //     .signers([SIMPLE_KEPPAIR]) // Add the loaded keypair as the signer
+    //     .rpc();
+    
+    //   console.log("Transaction signature:", tx);
+    // });
+    it("executed", async () => {
+      const tx = await program.methods.execute()
+        .accounts({
+          dest: SIMPLE_SIMPLE_TOKEN_ACCOUNT
+        })
+        .signers([SIMPLE_KEPPAIR])
+        .rpc();
+    
+      console.log("Transaction signature:", tx);
+    });
+
+    
+
+
+    
 });
